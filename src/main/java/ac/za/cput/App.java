2635a5bf3f7c7698b46ca16ccd8cf13b8ae75f93
Benitp <b.kriel34@gmail.com>
2019-02-14 03:20:17 +0200

    adding calculation for a basic math program

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -1,5 +1,7 @@
 package ac.za.cput;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -10,5 +12,16 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("---------------");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = in.nextInt();
+
+        System.out.println("Enter number");
+        int b = in.nextInt();
+        int result = a+b;
+
+        System.out.println("The sum of the two numbers is " + result );
+
     }
 }
